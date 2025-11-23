1491506691ceeba6754e354e7e67e1871cda63f9
toruko44 <toruko5022@gmail.com>
2025-11-23 20:54:56 +0900

    N100

---
+++ data/raw/gear/canon_n100rf.php
@@ -0,0 +1,21 @@
+<?php
+return[
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF 100mm F2.8L Macro IS USM', 'gear' => '', 'adapter' => '',  'extension1' => 'NA エクステンションリングA30II', 'extension2' => '',  'port' => 'NA E105Aマクロポート'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF 100mm F2.8L Macro IS USM', 'gear' => 'NA-CRF100-F', 'adapter' => 'NA A/120 マウントコンバーター35.5MFII',  'extension1' => '', 'extension2' => '',  'port' => 'NA マクロポートC100IS'],
+
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => '', 'extension2' => '',  'port' => 'NA E50A フラットポート'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40', 'extension2' => '',  'port' => 'NA N120/ワイドアングルコンバージョンポート×0.36(WACP-1B)'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40II', 'extension2' => '',  'port' => 'NA N120/ワイドアングルコンバージョンポート×0.36(WACP-1B)'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40', 'extension2' => '',  'port' => 'NA コンパクトワイドアングルコンバージョンポートX0.36'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40II', 'extension2' => '',  'port' => 'NA コンパクトワイドアングルコンバージョンポートX0.36'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => 'NA A/120 マウントコンバーター35.5MFII',  'extension1' => '', 'extension2' => '',  'port' => 'NA N120/ワイドアングルコンバージョンポート×0.36(WACP-1B)'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40', 'extension2' => '',  'port' => 'NA N120/フィッシュアイコンバージョンポート(FCP-1)'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 18-45mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40II', 'extension2' => '',  'port' => 'NA N120/フィッシュアイコンバージョンポート(FCP-1)'],
+
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 10-18mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40', 'extension2' => '',  'port' => 'NA 180Aガラスドームポート'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 10-18mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => '',  'extension1' => 'NA エクステンションリングA40II', 'extension2' => '',  'port' => 'NA 180Aガラスドームポート'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 10-18mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => 'NA A/120 マウントコンバーター35.5MFII',  'extension1' => '', 'extension2' => '',  'port' => 'NA 180ガラスドームポート'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 10-18mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => 'NA A/120 マウントコンバーター35.5MFII',  'extension1' => '', 'extension2' => '',  'port' => 'NA 200ドームポート'],
+        ['camera' => '', 'housing' => '','lens' => 'Canon RF-S 10-18mm F4.5-6.3 IS STM', 'gear' => 'NA-CR1845-Z', 'adapter' => 'NA A/120 マウントコンバーター35.5MFII',  'extension1' => '', 'extension2' => '',  'port' => 'NA 230ガラスドームポートII'],
+]
+?>
